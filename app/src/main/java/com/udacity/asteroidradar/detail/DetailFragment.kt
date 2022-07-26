ca9bf00aace15843cc269227ca641daca4e10e63
Ahmed <ahmed.nader1994@gmail.com>
2022-07-26 15:30:56 +0200

    first commit

---
+++ app/src/main/java/com/udacity/asteroidradar/detail/DetailFragment.kt
@@ -0,0 +1,36 @@
+package com.udacity.asteroidradar.detail
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import com.udacity.asteroidradar.R
+import com.udacity.asteroidradar.databinding.FragmentDetailBinding
+
+class DetailFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = FragmentDetailBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+
+        val asteroid = DetailFragmentArgs.fromBundle(arguments!!).selectedAsteroid
+
+        binding.asteroid = asteroid
+
+        binding.helpButton.setOnClickListener {
+            displayAstronomicalUnitExplanationDialog()
+        }
+
+        return binding.root
+    }
+
+    private fun displayAstronomicalUnitExplanationDialog() {
+        val builder = AlertDialog.Builder(activity!!)
+            .setMessage(getString(R.string.astronomica_unit_explanation))
+            .setPositiveButton(android.R.string.ok, null)
+        builder.create().show()
+    }
+}
